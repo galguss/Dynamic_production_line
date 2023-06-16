947995d297706b33679b70137aa0287e328c7d1e
galguss <gal.gus12@gmail.com>
2023-06-16 21:41:08 +0300

    Establishing infrastructure for the server

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = 5200;
+
+app.use(bodyParser.urlencoded({extended:false}));
+const path = require('path');
+app.use(express.static(path.join(__dirname ,'public')));
+app.use(express.json());
+app.use(morgan('dev'));
+app.set("view engine", "ejs");
+
+app.listen(port, (req, res) => {
+    console.log(`The Server is running....`);
+});
+
+mongoose.connect(process.env.mongoDB);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log(`Now listening on port ${port}`);
+});
